308db62e1c1d7e456b3b40d9d3b4a1dc2e2a5637
Isaac John Wesley <wonderviz@gmail.com>
2013-08-07 16:26:12 +0530

    added api for creating screenshot, added PhantomJS integration

--- makescreenshot.py
+++ makescreenshot.py
@@ -1,8 +1,58 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from flask.ext import restful
+from flask.ext.restful import reqparse
+
+import selenium.webdriver
+
+import PIL
+from PIL import Image
+import base64
+from io import BytesIO
 
 app = Flask(__name__)
+api = restful.Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('url', type=str)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+class TakeScreenshot(restful.Resource):
+    def get(self):
+        return {'hello': 'get'}
+
+    def post(self):
+        args = parser.parse_args()
+        finalink=take_screenshot('http://%s' %(args['url']))
+        return finalink
+
+
+def take_screenshot(url):
+    try:
+        webdriver = selenium.webdriver.PhantomJS()
+        webdriver.get(url)
+        webdriver.set_window_size(1280,800)
+        imagedata = webdriver.get_screenshot_as_base64()
+    finally:
+        webdriver.close()
+        webdriver.quit()
+
+    return process_screenshot(imagedata)
+
+def process_screenshot(base64_img):
+    basewidth = 220
+    img = Image.open(BytesIO(base64.b64decode(base64_img)))
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+    img = img.crop((0,0,basewidth,basewidth))
+    img.save('isaac.png')
+
+    finalurl="www.amazon.bucket.url-id-link.png"
+
+    return finalurl
+
+
+api.add_resource(TakeScreenshot, '/takescreenshot')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug= True)
