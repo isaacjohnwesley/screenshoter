41132d9f3f33c75e200ed552c3c57ebe010da4a1
Isaac John Wesley <wonderviz@gmail.com>
2013-08-08 22:10:24 +0530

    added settings file

--- makescreenshot.py
+++ makescreenshot.py
@@ -92,6 +92,9 @@ def upload_to_s3(upload_img):
 
 api.add_resource(TakeScreenshot, '/takescreenshot')
 
+#App Configuration
+app.config.from_pyfile('settings.py')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug= True)
